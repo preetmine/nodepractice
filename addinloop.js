0bd4983f5f31e2815ef192a459959907b2e891ce
Amanpreet Kaur <744678@student.tor.loc>
2019-11-22 15:52:13 -0500

    did

---
+++ addinloop.js
@@ -0,0 +1,16 @@
+//for loop
+var add=0;
+
+for(i=100;i>=1;i--){
+    add=add+i   
+}
+console.log("sum of numbers is",add)
+
+// while loop
+var sum=0;
+var ix=500
+while(ix>=114){
+    sum=sum+ix  
+    ix--; 
+}
+console.log("sum of numbers ",sum)
